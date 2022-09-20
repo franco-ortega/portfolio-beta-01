1adbd41fa0b0b4ecdc66060140110dce358e6333
Franco Ortega <francopdx@gmail.com>
2022-09-20 16:49:25 -0700

    create initial Header component with Nav

---
+++ components/header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.Header}>
+      Header
+      <nav>Nav</nav>
+    </header>
+  );
+};
+
+export default Header;
