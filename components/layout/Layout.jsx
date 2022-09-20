a6d86b4de67e29334e7fa268b7068fe40aa2db43
Franco Ortega <francopdx@gmail.com>
2022-09-20 16:51:30 -0700

    add Head and Header to Layout

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,7 +1,19 @@
+import Head from 'next/head';
+import Header from '../header/Header';
 import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
-  return <main className={styles.Layout}>{children}</main>;
+  return (
+    <div className={styles.Layout}>
+      <Head>
+        <title>Portfolio - Beta - 01</title>
+        <meta name='description' content='Practice portfolio site' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Header />
+      <main>{children}</main>
+    </div>
+  );
 };
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,6 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Portfolio - Beta - 01</title>
-        <meta name='description' content='Practice portfolio site' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <header>Head</header>
-
       <main>Boop</main>
 
       <footer>Feet</footer>
