604219b29c54daa77175cfbe0206fdcc0856e578
Franco Ortega <francopdx@gmail.com>
2022-09-21 11:13:28 -0700

    add links and resume to Nav

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -1,13 +1,24 @@
+import Link from 'next/link';
 import styles from './Nav.module.scss';
 
 const Nav = () => {
   return (
     <nav className={styles.Nav}>
       <ul>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>About</li>
-        <li>Resume</li>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/projects'>Projects</Link>
+        </li>
+        <li>
+          <Link href='/about'>About</Link>
+        </li>
+        <li>
+          <a href='/Resume_FrancoOrtega.pdf' rel='noreferrer' target='_blank'>
+            Resume
+          </a>
+        </li>
       </ul>
     </nav>
   );
