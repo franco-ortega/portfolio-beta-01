ad28315603793ddd1d9cf6d0c6314b0dae986bcd
Franco Ortega <francopdx@gmail.com>
2022-09-21 08:11:23 -0700

    create landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
+import { useState } from 'react';
 import Layout from '../components/layout/Layout';
 import LayoutInner from '../components/layoutInner/LayoutInner';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const [home, setHome] = useState(false);
+
+  const onWelcomeClick = () => {
+    console.log('welcome!!');
+    setHome(true);
+  };
+
+  if (!home)
+    return (
+      <div>
+        <h1>Franco Ortega</h1>
+        <h2>Software Engineer</h2>
+        <button onClick={onWelcomeClick}>Welcome</button>
+      </div>
+    );
+
   return (
     <Layout>
       <LayoutInner>
