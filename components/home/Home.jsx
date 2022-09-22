3d85a3dfff99bc44ff916ec61fd65c5a27388dd8
Franco Ortega <francopdx@gmail.com>
2022-09-22 16:40:44 -0700

    adjust grid for Home across screen sizes

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div className={styles.Home}>
       <h3>Hello!</h3>
+      {/* <p>Testing, one two, three...</p> */}
 
       <div>
         I&apos;m a software engineer who loves to create intuitive interfaces
@@ -22,6 +23,23 @@ const Home = () => {
         Optimism guides my efforts while practicality ensures that my goals are
         brought to fruition, and helping others brings me great joy.
       </div>
+      {/* <div>
+        I&apos;m a software engineer who loves to create intuitive interfaces
+        and build out features across the stack. I value:
+        <ul>
+          <li>
+            well-documented and efficient processes that offer both structure
+            and flexibility
+          </li>
+          <li>clear and kind communication</li>
+          <li>cross-functional collaboration</li>
+          <li>
+            supportive environments that foster diversity and nurture growth
+          </li>
+        </ul>
+        Optimism guides my efforts while practicality ensures that my goals are
+        brought to fruition, and helping others brings me great joy.
+      </div> */}
     </div>
   );
 };
