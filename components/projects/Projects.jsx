8efab9561d59371605c6a04cdeb1366870611ca6
Franco Ortega <francopdx@gmail.com>
2022-09-22 18:29:00 -0700

    add grid to Layout

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -5,6 +5,14 @@ const Projects = () => {
     <div className={styles.Projects}>
       <h2>Projects</h2>
       <p>These are my projects</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia dicta
+        eligendi et incidunt maiores architecto provident ut quod atque omnis
+        assumenda iure, deserunt reprehenderit expedita id libero accusantium
+        enim aliquid harum praesentium obcaecati numquam perferendis? Corporis
+        rerum exercitationem voluptas itaque quisquam id nostrum? Ea ipsa
+        beatae, tenetur laborum possimus dolor?
+      </p>
     </div>
   );
 };
