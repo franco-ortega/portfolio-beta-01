b6fab58bfbe605c3818995003bdef08ba38498c5
Franco Ortega <francopdx@gmail.com>
2022-09-26 16:53:17 -0700

    display project sample data in Projects component with Project components

--- data/projects.js
+++ data/projects.js
@@ -24,3 +24,5 @@ const projects = [
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia dicta eligendi et incidunt maiores architecto provident ut quod atque omnis assumenda iure, deserunt reprehenderit expedita id libero accusantium enim aliquid harum praesentium obcaecati numquam perferendis? Corporis rerum exercitationem voluptas itaque quisquam id nostrum? Ea ipsa beatae, tenetur laborum possimus dolor?'
   }
 ];
+
+export default projects;
