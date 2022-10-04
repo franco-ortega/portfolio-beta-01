35bdfd44515667690091257399773fbc80bb0553
Franco Ortega <francopdx@gmail.com>
2022-10-03 18:53:21 -0700

    map through learnings

--- components/projects/ProjectDetails.jsx
+++ components/projects/ProjectDetails.jsx
@@ -1,16 +1,18 @@
 import displayTech from '../../utils/displayTech';
 import styles from './ProjectDetails.module.scss';
 
-const ProjectDetails = ({
-  data
-  // title, tech, description, learnings
-}) => {
+const ProjectDetails = ({ data }) => {
   return (
     <div className={styles.ProjectDetails}>
       <h2>Title: {data.title}</h2>
       <p>Tech: {displayTech(data.tech)}</p>
       <p>Description: {data.description}</p>
-      <p>Learnings: {data.learnings}</p>
+      <ul>
+        Learnings:
+        {data.learnings.map((learning, i) => (
+          <li key={i}>{learning}</li>
+        ))}
+      </ul>
     </div>
   );
 };
--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -7,18 +7,9 @@ export default function ProjectDetailsPage({ data }) {
 
 export async function getServerSideProps(context) {
   try {
-    console.log('PARAMS: ', context.params.project);
-
-    const data = projectData.filter(
+    const [data] = projectData.filter(
       (project) => project.path === context.params.project
-    )[0];
-    console.log(data);
-
-    // const raw = await db
-    //   .collection('trays')
-    //   .findOne({ _id: ObjectId(context.params.id) });
-
-    // const data = await JSON.parse(JSON.stringify(raw));
+    );
 
     return {
       props: { isConnected: true, data }
