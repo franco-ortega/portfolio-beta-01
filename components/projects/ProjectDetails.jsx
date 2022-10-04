2732eda98f211a6eb8e8ed44b7d0f6eda5095a09
Franco Ortega <francopdx@gmail.com>
2022-10-03 19:03:05 -0700

    style ProjectDetails layout

--- components/projects/ProjectDetails.jsx
+++ components/projects/ProjectDetails.jsx
@@ -4,15 +4,28 @@ import styles from './ProjectDetails.module.scss';
 const ProjectDetails = ({ data }) => {
   return (
     <div className={styles.ProjectDetails}>
-      <h2>Title: {data.title}</h2>
-      <p>Tech: {displayTech(data.tech)}</p>
-      <p>Description: {data.description}</p>
-      <ul>
-        Learnings:
-        {data.learnings.map((learning, i) => (
-          <li key={i}>{learning}</li>
-        ))}
-      </ul>
+      <h2>Project: {data.title}</h2>
+      <p>
+        <h3>Tech</h3> {displayTech(data.tech)}
+      </p>
+      <p>
+        <h3>Description</h3>
+        {data.description}
+      </p>
+      <p>
+        <h3>Learnings</h3>
+        <ul>
+          {data.learnings.map((learning, i) => (
+            <li key={i}>{learning}</li>
+          ))}
+        </ul>
+      </p>
+
+      <div>
+        <p>Website</p>
+        {' - '}
+        <p>GitHub</p>
+      </div>
     </div>
   );
 };
