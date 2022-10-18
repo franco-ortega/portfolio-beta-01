3b6b43fe6b3df80e3f6f063978a2c4a499666f50
Franco Ortega <francopdx@gmail.com>
2022-10-17 18:32:51 -0700

    create TestNav

---
+++ components/test/TestNav.jsx
@@ -0,0 +1,24 @@
+const TestNav = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Link #1</li>
+          <li>Link #2</li>
+          <li>Link #3</li>
+          <li>Link #4</li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          <li>Link A</li>
+          <li>Link B</li>
+          <li>Link C</li>
+          <li>Link D</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default TestNav;
